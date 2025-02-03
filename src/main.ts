73f0818d5de836b740ef561dcff5e7430b0517d0
kphed <kpcrypto69@gmail.com>
2025-02-03 14:43:46 -0500

    Run container setup in main process

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-import { loadValidatedConfig } from './shared/config';
+import { Config, loadValidatedConfig } from './shared/config';
 import { checkNodeIsUpToDate } from './version';
 import { assignPorts } from './utils/container';
+import { ContainerManager } from './orchestration/docker';
 
 const configPath = process.env.INFERNET_CONFIG_PATH ?? 'config.json';
 
 (async () => {
-  let config;
+  let config: Config;
 
   try {
     config = await loadValidatedConfig(configPath);
@@ -18,6 +19,14 @@ const configPath = process.env.INFERNET_CONFIG_PATH ?? 'config.json';
     await checkNodeIsUpToDate();
 
     config.containers = assignPorts(config.containers);
+    const containerManager = new ContainerManager(
+      config.containers,
+      config.docker,
+      config.startup_wait,
+      config.manage_containers
+    );
+
+    await containerManager.setup();
   } catch (err) {
     throw `Config file validation failed: ${err}`;
   }
