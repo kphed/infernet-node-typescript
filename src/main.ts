dc8c35e436e1e7868ceb94d1d3513b2d57b45fa6
kphed <kpcrypto69@gmail.com>
2025-02-17 10:02:13 -0500

    Add chain-specific task and chain-enabled vars

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,19 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-import { Config, loadValidatedConfig } from './shared/config';
+import { Config, loadValidatedConfig, ConfigWallet } from './shared/config';
 import { checkNodeIsUpToDate } from './version';
 import { assignPorts } from './utils/container';
 import { ContainerManager } from './orchestration/docker';
 import { DataStore } from './orchestration/store';
 import { Orchestrator } from './orchestration/orchestrator';
 import { ContainerLookup } from './chain/containerLookup';
+import { ChainProcessor } from './chain/processor';
+import { Wallet } from './chain/wallet';
+import { RPC } from './chain/rpc';
+import { Registry } from './chain/registry';
+import { add0x } from './utils/helpers';
+import { WalletChecker } from './chain/walletChecker';
 
 const configPath = process.env.INFERNET_CONFIG_PATH ?? 'config.json';
 
@@ -21,6 +27,7 @@ const configPath = process.env.INFERNET_CONFIG_PATH ?? 'config.json';
 
     await checkNodeIsUpToDate();
 
+    const chainEnabled = config.chain.enabled;
     config.containers = assignPorts(config.containers);
     const containerManager = new ContainerManager(
       config.containers,
@@ -37,6 +44,33 @@ const configPath = process.env.INFERNET_CONFIG_PATH ?? 'config.json';
 
     const orchestrator = new Orchestrator(containerManager, store);
     const containerLookup = new ContainerLookup(config.containers);
+
+    // Initialize chain-specific tasks.
+    let process: ChainProcessor;
+    let wallet: Wallet;
+    let chainId: number;
+
+    if (chainEnabled) {
+      const walletConfig = config.chain.wallet as ConfigWallet;
+      const rpcUrl = config.chain.rpc_url as string;
+      const registryAddress = config.chain.registry_address as string;
+      const privateKey = add0x(walletConfig.private_key as string);
+      const rpc = new RPC(rpcUrl, privateKey);
+      chainId = await rpc.get_chain_id();
+      const registry = new Registry(
+        rpc,
+        RPC.get_checksum_address(registryAddress)
+      );
+      const paymentAddress = walletConfig.payment_address
+        ? RPC.get_checksum_address(walletConfig.payment_address)
+        : undefined;
+      const walletChecker = new WalletChecker(
+        rpc,
+        registry,
+        config.containers,
+        paymentAddress
+      );
+    }
   } catch (err) {
     throw `Config file validation failed: ${err}`;
   }
