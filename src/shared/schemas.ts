999be1cb13289ad58ce7dcbc3cc84f6c4eaf773e
kphed <kpcrypto69@gmail.com>
2025-02-18 16:02:20 -0500

    Create shared/schemas file for frequently-used Zod schemas, update config

---
+++ src/shared/schemas.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { isAddress } from 'viem';
+
+export const NumberSchema = z.number();
+
+export const StringSchema = z.string();
+
+export const DefaultNumberSchema = (value: number) =>
+  NumberSchema.default(value);
+
+export const DefaultStringSchema = (value: string) =>
+  StringSchema.default(value);
+
+export const DefaultBooleanSchema = (value: boolean) =>
+  z.boolean().default(value);
+
+export const AddressStringSchema = StringSchema.length(42).refine(
+  (address: string) => isAddress(address, { strict: false })
+);
+
+export const ChecksumAddressStringSchema = StringSchema.length(42).refine(
+  (address: string) => isAddress(address)
+);
+
+export const StrictObjectSchema = (value: any) => z.object(value).strict();
